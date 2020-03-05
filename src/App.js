a523aedd287c8157b3576940acc56cca9d073bd2
Tomi Tarvainen <tomi.tarvainen@alten.fi>
2020-03-05 14:12:01 +0200

    Add TabBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import TabBar from './components/TabBar';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <TabBar />
         <p>
           Betting
         </p>
